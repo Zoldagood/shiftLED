44fb06c208f63e4f7ba50d151ca9028b3d77e7b9
jh <silverum87@gmail.com>
2018-10-17 17:12:45 +0900

    181017 C# Serial통신 엄장현

---
+++ Csharp/Serial_WinForm/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Serial_WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.Open = new System.Windows.Forms.Button();
+            this.Close = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Device_comboBox = new System.Windows.Forms.ComboBox();
+            this.BaudRate_comboBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Clear = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Send = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(246, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(54, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button2.Location = new System.Drawing.Point(339, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(54, 46);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button3.Location = new System.Drawing.Point(432, 74);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(54, 46);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button4.Location = new System.Drawing.Point(525, 74);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(54, 46);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button5.Location = new System.Drawing.Point(246, 141);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(54, 46);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button6.Location = new System.Drawing.Point(339, 141);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(54, 46);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button7.Location = new System.Drawing.Point(432, 141);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(54, 46);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button8.Location = new System.Drawing.Point(525, 141);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(54, 46);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(15, 91);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(93, 29);
+            this.Open.TabIndex = 8;
+            this.Open.Text = "Open Port";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // Close
+            // 
+            this.Close.Location = new System.Drawing.Point(127, 91);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(95, 29);
+            this.Close.TabIndex = 9;
+            this.Close.Text = "Close Port";
+            this.Close.UseVisualStyleBackColor = true;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM4";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(15, 193);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(565, 194);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Device";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 15);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "BaudRate";
+            // 
+            // Device_comboBox
+            // 
+            this.Device_comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Device_comboBox.FormattingEnabled = true;
+            this.Device_comboBox.Location = new System.Drawing.Point(89, 12);
+            this.Device_comboBox.Name = "Device_comboBox";
+            this.Device_comboBox.Size = new System.Drawing.Size(133, 23);
+            this.Device_comboBox.TabIndex = 13;
+            // 
+            // BaudRate_comboBox
+            // 
+            this.BaudRate_comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.BaudRate_comboBox.FormattingEnabled = true;
+            this.BaudRate_comboBox.Location = new System.Drawing.Point(89, 48);
+            this.BaudRate_comboBox.Name = "BaudRate_comboBox";
+            this.BaudRate_comboBox.Size = new System.Drawing.Size(133, 23);
+            this.BaudRate_comboBox.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(243, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 17);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Port Label";
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(127, 151);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(95, 29);
+            this.Clear.TabIndex = 16;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(245, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(241, 25);
+            this.textBox1.TabIndex = 17;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // Send
+            // 
+            this.Send.Location = new System.Drawing.Point(504, 14);
+            this.Send.Name = "Send";
+            this.Send.Size = new System.Drawing.Size(76, 23);
+            this.Send.TabIndex = 18;
+            this.Send.Text = "Send";
+            this.Send.UseVisualStyleBackColor = true;
+            this.Send.Click += new System.EventHandler(this.Send_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(598, 399);
+            this.Controls.Add(this.Send);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BaudRate_comboBox);
+            this.Controls.Add(this.Device_comboBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.Open);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.Button Close;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox Device_comboBox;
+        private System.Windows.Forms.ComboBox BaudRate_comboBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Send;
+    }
+}
+
---
+++ Csharp/Serial_WinForm/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Serial_WinForm
+{
+    public partial class Form1 : Form
+    {
+        String[] baudrate;
+        String[] comports;
+        public Form1()
+        {
+            InitializeComponent();
+            IsSerialSts();
+            try {
+                
+                comports = SerialPort.GetPortNames();
+                baudrate = new String[] { "9600", "115200" };
+                BaudRate_comboBox.Items.AddRange(baudrate);
+                Device_comboBox.Items.AddRange(comports);
+
+            }
+            catch(Exception)
+            {
+                richTextBox1.AppendText("설정을 확인하세요\n");
+            }
+
+        }
+        private void IsSerialSts()
+        {
+            if (serialPort1.IsOpen)
+            {
+                Open.Enabled = false;
+                Close.Enabled = true;
+               
+            }
+            else
+            {
+                Open.Enabled = true;
+                Close.Enabled = false;
+               
+            }
+        }
+           
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = Device_comboBox.SelectedItem.ToString();
+                serialPort1.BaudRate = int.Parse(BaudRate_comboBox.SelectedItem.ToString());
+                serialPort1.Open();
+                IsSerialSts();
+            }
+            catch(Exception)
+            {
+                richTextBox1.AppendText("설정을 확인하세요\n");
+            }
+
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            IsSerialSts();
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.WriteLine("1");
+                richTextBox1.ScrollToCaret();
+            }
+            catch(Exception)
+            {
+                richTextBox1.AppendText("포트가 닫혀있습니다.\n"); 
+            }
+               
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try { 
+            serialPort1.WriteLine("2");
+            richTextBox1.ScrollToCaret();
+            }
+            catch (Exception)
+            {
+                richTextBox1.AppendText("포트가 닫혀있습니다.\n");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try { 
+            serialPort1.WriteLine("3");
+            richTextBox1.ScrollToCaret();
+            }
+            catch (Exception)
+            {
+                richTextBox1.AppendText("포트가 닫혀있습니다.\n");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try { 
+            serialPort1.WriteLine("4");
+            richTextBox1.ScrollToCaret();
+            }
+            catch (Exception)
+            {
+                richTextBox1.AppendText("포트가 닫혀있습니다.\n");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try { 
+            serialPort1.WriteLine("5");
+            richTextBox1.ScrollToCaret();
+            }
+            catch (Exception)
+            {
+                richTextBox1.AppendText("포트가 닫혀있습니다.\n");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try { 
+            serialPort1.WriteLine("6");
+            richTextBox1.ScrollToCaret();
+            }
+            catch (Exception)
+            {
+                richTextBox1.AppendText("포트가 닫혀있습니다.\n");
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            try { 
+            serialPort1.WriteLine("7");
+            richTextBox1.ScrollToCaret();
+            }
+            catch (Exception)
+            {
+                richTextBox1.AppendText("포트가 닫혀있습니다.\n");
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            try { 
+            serialPort1.WriteLine("8");
+            richTextBox1.ScrollToCaret();
+            }
+            catch (Exception)
+            {
+                richTextBox1.AppendText("포트가 닫혀있습니다.\n");
+            }
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void Send_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.WriteLine(textBox1.Text);
+            }
+            catch(Exception)
+            {
+                richTextBox1.AppendText("포트가 닫혀있습니다.\n");
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            try
+            {
+                if (e.KeyChar == '\r')
+                {
+                    serialPort1.WriteLine(textBox1.Text);
+                }
+            }
+            catch (Exception)
+            {
+                richTextBox1.AppendText("포트가 닫혀있습니다.\n");
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string getdata;
+                getdata = serialPort1.ReadLine();
+                richTextBox1.AppendText(getdata);
+                richTextBox1.ScrollToCaret();
+            }
+            catch(Exception)
+            {
+
+            }
+        }
+    }
+}
