02a582e015213df2845631013110eb8f9a93e5a6
jh <silverum87@gmail.com>
2018-11-07 14:29:06 +0900

    18.11.07 평가 1번 0.3초 LED 8개 toggle

---
+++ AVR/toggle_LED/toggle_LED/main.c
@@ -0,0 +1,24 @@
+/*
+ * toggle_LED.c
+ *
+ * Created: 2018-11-07 오후 2:20:19
+ * Author : kccistc
+ */ 
+
+#define F_CPU 16000000L
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    DDRC = 0xff;
+	PORTC = 0xff;
+    while (1) 
+    {
+		PORTC = 0xff;
+		_delay_ms(300);
+		PORTC = 0x00;
+		_delay_ms(300);
+    }
+}
+
